fe0639c05ee7dc8abc41c8ddae60bade9d366376
danielbricio <danielbricio10@gmail.com>
2025-05-21 11:41:37 -0300

    Atualizações no codigo js em modo noturno e claro.

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const btn = document.getElementById('toggle-dark');
+
 btn.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
-  if(document.body.classList.contains('dark-mode')) {
+  if (document.body.classList.contains('dark-mode')) {
     localStorage.setItem('theme', 'dark');
   } else {
     localStorage.setItem('theme', 'light');
   }
+  updateButtonText();
 });
 
-if(localStorage.getItem('theme') === 'dark') {
+function updateButtonText() {
+  if (document.body.classList.contains('dark-mode')) {
+    btn.textContent = 'Modo Claro';
+  } else {
+    btn.textContent = 'Modo Noturno';
+  }
+}
+
+if (localStorage.getItem('theme') === 'dark') {
   document.body.classList.add('dark-mode');
 }
+updateButtonText();
